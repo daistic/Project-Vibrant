d17097f273224720d3be7584f0868907c8ba1c73
daistic <matotsuper@gmail.com>
2025-11-07 08:41:05 +0700

    add: puzzle fragment dragging

---
+++ Entities/Puzzle Fragment/PuzzleFragment.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var sprite: CompressedTexture2D
+@export var polygon_points: PackedVector2Array
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_polygon_2d: CollisionPolygon2D = $CollisionPolygon2D
+
+var dragging: bool = false
+var offset: Vector2 = Vector2(0, 0)
+
+func _ready() -> void:
+	sprite_2d.texture = sprite
+	collision_polygon_2d.polygon = polygon_points
+
+func _process(_delta: float) -> void:
+	if dragging:
+		position = get_global_mouse_position() - offset
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event.is_action_pressed("click_puzzle"):
+		dragging = true
+		offset = get_global_mouse_position() - global_position
+	
+	if event.is_action_released("click_puzzle"):
+		dragging = false
